3e285540e89c4e5fea5bc8e68819ec480dd4f997
AbdelrahmanSamirz <shekaba@gmail.com>
2026-03-15 05:46:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_Session06_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
